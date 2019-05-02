b7de9441f25f9115c3a629bc5b77da91d3bfcb1a
unknown <angyalva16@gmail.com>
2019-05-01 22:11:21 -0500

    Se modifican archivos para la visualizacion de los links en consola

--- src/validatePath.js
+++ src/validatePath.js
@@ -1,56 +1,51 @@
 let fs = require("fs");
 let path = require("path");
 
-const directoryFileList = [];
+// const directoryFileList = [];
 
 console.log("Inicio");
 
-const validatePathExistWithExtensionMd = (Myroute) =>{
-  const extension = path.extname(Myroute);
-  if(extension === ".md"){
-    return true;
-  }else{
-    return false;
-  };
-};
-// console.log(validatePathExistWithExtensionMd("README.md"));
+// const validatePathExistWithExtensionMd = (Myroute) =>{
+//   const extension = path.extname(Myroute);
+//   if(extension === ".md"){
+//     return true;
+//   }else{
+//     return false;
+//   };
+// };
+// // console.log(validatePathExistWithExtensionMd("README.md"));
 
-const validatePathAbsolute = (Myroute) =>{
-  return path.isAbsolute(Myroute);
-};
-//Relativa -> absoluta 
-const transformRelativePath = (Myroute) =>{
-  return path.resolve(Myroute);
-};
+// const validatePathAbsolute = (Myroute) =>{
+//   return path.isAbsolute(Myroute);
+// };
+// //Relativa -> absoluta 
+// const transformRelativePath = (Myroute) =>{
+//   return path.resolve(Myroute);
+// };
 
-const checkIfTheFileExistInADirectiry = (Myroute) => {
-  return new Promise((resolve, reject) => {
-    fs.access(Myroute,"utf-8", (err) => {
-      return err ? reject(err) : resolve('exists');
-    });
-});
-};
+// const checkIfTheFileExistInADirectiry = (Myroute) => {
+//   return new Promise((resolve, reject) => {
+//     fs.access(Myroute,"utf-8", (err) => {
+//       return err ? reject(err) : resolve('exists');
+//     });
+// });
+// };
 
-const readFileMd = (Myroute) => {
-  return new Promise((resolve, reject) => {
-      fs.readFile(Myroute, "utf-8", (err, data) => {
-        return err ? reject(err) : resolve(data);
-      });
-  });
-};
 
-const readDirectory = (Myroute) => {
-  return new Promise((resolve, reject) => {
-    fs.readdir(Myroute, (err, files) => {
-      if(err){
-        reject(err);
-        return;
-      }
-      resolve(true);
-    });
-  });
-};
 
+// const readDirectory = (Myroute) => {
+//   return new Promise((resolve, reject) => {
+//     fs.readdir(Myroute, (err, files) => {
+//       if(err){
+//         reject(err);
+//         return;
+//       }
+//       resolve(true);
+//       console.log(files);
+//     });
+//   });
+// };
+// console.log(readFileMd("./"));
 // const filterDirectoryMarkdownFiles = (files) => {
 //   files.forEach((element) => {
 //     if (path.extname(element) === '.md') {
@@ -60,13 +55,24 @@ const readDirectory = (Myroute) => {
 //   });
 // };
 
-console.log("Final");
+// console.log("Final");
 
-module.exports ={
-  validatePathExistWithExtensionMd: validatePathExistWithExtensionMd,
-  validatePathAbsolute: validatePathAbsolute,
-  transformRelativePath: transformRelativePath,
-  checkIfTheFileExistInADirectiry: checkIfTheFileExistInADirectiry,
-  readFileMd: readFileMd,
-  readDirectory: readDirectory
-};
+module.exports = (Myroute, options) =>{
+  // const readFileMd = (Myroute, options) => {
+    return new Promise((resolve, reject) => {
+      fs.readFile(Myroute, function(err, data){
+        if (err){
+          return reject(err);
+        }
+        resolve(data.toString());
+        // console.log(data.toString());
+      });
+    });
+  };
+  // validatePathExistWithExtensionMd(Myroute),
+  // validatePathAbsolute(Myroute),
+  // transformRelativePath(Myroute),
+  // checkIfTheFileExistInADirectiry(Myroute),
+  // readFileMd(Myroute)
+  // readDirectory(Myroute)
+// };
